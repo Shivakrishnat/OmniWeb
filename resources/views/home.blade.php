75275588ceaf04e535d5decd2798134f153787b3
Shivakrishnat <shivakrishnat095@gmail.com>
2015-03-02 07:18:15 +0530

    OmniWeb
    OmniWeb

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Home</div>
+
+				<div class="panel-body">
+					<p>You are logged in!</p>
+					<a href="#"><button type="Download" class="btn btn-primary btn-lg" style="background:rgb(242,48,48);border-color:#fff;">Download</button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
